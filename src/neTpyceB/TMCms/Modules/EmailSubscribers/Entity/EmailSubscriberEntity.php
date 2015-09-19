9d387bdb98c8f57fd847d68eebeccb3c3132434e
neTpyceB <pvman@inbox.ru>
2015-09-19 23:45:20 +0300

    Entity EmailSubscriberEntity created with general properties

---
+++ src/neTpyceB/TMCms/Modules/EmailSubscribers/Entity/EmailSubscriberEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace neTpyceB\TMCms\Modules\EmailSubscribers;
+
+use neTpyceB\TMCms\Modules\Entity;
+
+/**
+ * Class EmailSubscriberEntity
+ * @package neTpyceB\TMCms\Modules\EmailSubscribers
+ */
+class EmailSubscriberEntity extends Entity
+{
+    protected $db_table = 'm_email_subscribers';
+}
