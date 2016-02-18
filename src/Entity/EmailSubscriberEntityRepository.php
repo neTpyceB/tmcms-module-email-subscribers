de270edbfd41a979778b9dc4a309a37ce5b24081
Bogdan Delta <delta@devp.lv>
2016-02-18 16:13:36 +0200

    Added table structure property and removed DB directory

--- src/Entity/EmailSubscriberEntityRepository.php
+++ src/Entity/EmailSubscriberEntityRepository.php
@@ -11,4 +11,31 @@ use TMCms\Orm\EntityRepository;
 class EmailSubscriberEntityRepository extends EntityRepository
 {
     protected $db_table = 'm_email_subscribers';
+    protected $table_structure = [
+        'fields' => [
+            'ip' => [
+                'type' => 'varchar',
+                'length' => 15
+            ],
+            'ts' => [
+                'type' => 'int',
+                'unsigned' => true
+            ],
+            'user_id' => [
+                'type' => 'index'
+            ],
+            'email' => [
+                'type' => 'varchar'
+            ],
+            'receive_news' => [
+                'type' => 'bool'
+            ],
+            'receive_ads' => [
+                'type' => 'bool'
+            ],
+            'notes' => [
+                'type' => 'varchar'
+            ]
+        ]
+    ];
 }
