b96cb46438b8ecb6efa96163ca8ce87113d3c159
neTpyceB <pvman@inbox.ru>
2016-02-24 22:37:22 +0200

    Structure for table

--- src/Entity/EmailSubscriberEntityRepository.php
+++ src/Entity/EmailSubscriberEntityRepository.php
@@ -11,4 +11,32 @@ use TMCms\Orm\EntityRepository;
 class EmailSubscriberEntityRepository extends EntityRepository
 {
     protected $db_table = 'm_email_subscribers';
+    protected $table_structure = [
+        'fields' => [
+            'ip' => [
+                'type' => 'varchar',
+                'unsigned' => true,
+                'length' => '15',
+            ],
+            'ts' => [
+                'type' => 'varchar',
+                'unsigned' => true,
+            ],
+            'user_id' => [
+                'type' => 'index',
+            ],
+            'email' => [
+                'type' => 'varchar',
+            ],
+            'notes' => [
+                'type' => 'varchar',
+            ],
+            'receive_news' => [
+                'type' => 'bool',
+            ],
+            'receive_ads' => [
+                'type' => 'bool',
+            ],
+        ],
+    ];
 }
