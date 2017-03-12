b0cfb66eb39e9d6372855bb95356167dbf22a874
neTpyceB <pvman@inbox.ru>
2017-03-12 21:23:15 +0200

    Menu and icon

---
+++ src/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Email Subscribers',
+    'icon' => 'envelope-letter',
+    'items' => [
+        '_default' => [
+            'title' => 'Main'
+        ],
+    ]
+];
